b981cf9b7a60839c351622fedd37ab939a094c09
sansela <santosh.sela@gmail.com>
2025-06-01 23:41:34 -0400

    jwt project completion

--- webdev/week-6/jwt-fe-be/index.js
+++ webdev/week-6/jwt-fe-be/index.js
@@ -13,7 +13,6 @@ app.post('/signup', (req, res) => {
     users.push({
         username, password
     })
-    console.log(users);
     res.status(201).send({
         message: "User Signed up successfully"
     })
@@ -24,7 +23,6 @@ app.post('/signin', (req, res) => {
     const password = req.body.password
 
     const user = users.find(user => (user.username === username && user.password === password))
-    console.log(user)
 
 
     if (user) {
@@ -38,6 +36,33 @@ app.post('/signin', (req, res) => {
         })
     }
 
+})
+
+function auth(req, res, next) {
+    try {
+        const authHeader = req.headers.authorization
+        const userData = jwt.verify(authHeader, JWT_SECRET)
+        const username = userData.username;
+        const user = users.find(user => user.username === username)
+        const password = user.password
+        req.password = password
+        next()
+    } catch (err) {
+        console.log("auth Error: ", err)
+        return res.status(403).json({
+            message: "Authencation Error"
+        })
+    }
+}
+
+app.get('/me', auth, (req, res) => {
+    return res.json({
+        password: req.password
+    })
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 app.listen(3000, () => {
